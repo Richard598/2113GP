b27584ad627fbcc4f645fc09af7ad1635255f24a
PENG Bo <2707433395@qq.com>
2025-04-26 13:43:50 +0800

    Update terminal.h

--- terminal.h
+++ terminal.h
@@ -1,1 +1,21 @@
+#pragma once
 
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string>
+#include<map>
+#include <random>
+#include <chrono>
+#include <thread>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+#include<algorithm>
+
+using namespace std;
+
+void initTerminal(struct termios &origTermios);
+void restoreTerminal(struct termios origTermios);
