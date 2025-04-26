0b0ca8c115f5995a5f745f454fffa11725d3f777
Richard598 <q23287378772@icloud.com>
2025-04-26 13:23:29 +0800

    Update main.h

--- main.h
+++ main.h
@@ -1,1 +1,20 @@
+#pragma once
 
+#include"game.h"
+
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string>
+#include<map>
+#include <random>
+#include <chrono>
+#include <thread>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+#include<algorithm>
+
+using namespace std;
